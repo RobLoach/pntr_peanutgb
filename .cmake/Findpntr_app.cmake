5d398afe7319e7c0b52f7f4521fcdbcfd558b327
Rob Loach <robloach@gmail.com>
2023-09-19 16:08:45 -0400

    Update

--- .cmake/Findpntr_app.cmake
+++ .cmake/Findpntr_app.cmake
@@ -3,6 +3,6 @@ include(FetchContent)
 FetchContent_Declare(
     pntr_app
     GIT_REPOSITORY https://github.com/robloach/pntr_app.git
-    GIT_TAG a94175789c417d6f09b64184f686a9510f70c54e
+    GIT_TAG fe76bb86124340d74a5e967565491af4898691bf
 )
 FetchContent_MakeAvailable(pntr_app)
